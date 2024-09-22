dca5b8d2f692623553d19bf294aabedfe5fd3b76
Dylan Cunningham <dylanitwork@gmail.com>
2024-09-22 20:00:41 -0300

    - wrote main validation functions - convert < > function - regex pattern added - TODO's added

--- main.cpp
+++ main.cpp
@@ -1,152 +1,61 @@
 #include <iostream>
+#include <sys/stat.h>
 #include <string>
 #include <fstream>
+#include <regex>
 
 
 // TODO: - research criteria for valid Windows filename (running on mac is requirement)
 
-//           <<PSEUDOCODE>>
-
-// step 1: Implement file name & path validation--
-// check if given file path is valid. if not, it
-// return false; otherwise, it returns true
-
-    //FUNCTION isValidPath(filePath)
-        //if filepath is not valid
-            //return false
-        //ENDIF
-        //RETURN true
-    //end function
-
-// STEP 2: Read in a C++ source file --
-// validate the path. if valid, open
-// file and read contents into variable
-// 'sourceCode' then close.
-
-    //FUNCTION readFile(filepath)
-        //IF isvalidpath(filepath) is false
-            //PRINT invalid path
-            //RETURN
-        //ENDIF
-        //OPEN file at filepath FOR reading
-        //READ file content into sourceCode
-        //CLOSE file
-        //RETURN sourcecode
-    //ENDFUNCTION
-
-// step 3: Convert < and > symbols
-    //FUNCTION convertSymbols(sourceCode)
-        //REPLACE all '<' in sourceCode WITH '&lt;'
-        //REPLACE all '>' in sourceCode WITH '&gt;'
-        //RETURN sourceCode
-    //END FUNCTION
-
-// step 4: Insert <PRE> and </PRE> tags
-    //FUNCTION addPreTags(sourceCode)
-        //RETURN "<PRE>" + sourceCode + "</PRE>"
-    //ENDFUNCTION
-
-// step 5: output the modded file as HTML file
-    //FUNCTION writeFile(filePath, content)
-        //IF isValidPath(filePath) is false
-            //PRINT invalid
-            //RETURN
-        //ENDIF
-        //OPEN file at filepath FOR writing
-        //WRITE content to file
-        //CLOSE file
-    //END function
-
-    //Main program
-    //INPUT inputfilepath
-    //INPUT outputfilepath
-
-    //sourceCode = readFile(inputFilePath)
-    //IF sourceCode is not empty
-        //modifiedCode = convertSymbols(sourceCode)
-        //htmlContent = addPreTags(modifiedCode)
-        //writeFile(outputFilePath, htmlContent)
-        //PRINT "File conversion successful."
-    //ELSE
-        //PRINT failed to read
-    //ENDIF
-//END
 
 
-using namespace std;
+// Validate file name + path
+bool isValidFileName(const std::string& fileName) {
+    //
+    //Windows file name restrictions REGEX
+    // Source: https://kb.unca.edu/kb/file-name-best-practices#:~:text=Windows%20can%20handle%20file%20names,things%20known%20as%20system%20variables.
+    //
+    const std::regex pattern(R"(^[^<>:"/\\|?*\x00-\x1F]+$)");
+    return std::regex_match(fileName, pattern) && fileName.length() <= 259;
+}
 
-void openCloseStreams();
-void openPrint();
-void openWrite();
+bool isValidPath(const std::string& path) {
+    struct stat buffer;
+    return (stat(path.c_str(), &buffer) == 0);
+}
 
+// Convert < and > symbols
+std::string convertSymbols(const std::string& line) {
+    std::string convertedLine;
+    for (char c : line) {
+        if (c == '<') {
+            convertedLine += "lt;";
+        } else if (c == '>') {
+            convertedLine += "&gt;";
+        } else {
+            convertedLine += c;
+        }
+    }
+    return convertedLine;
+}
 
 int main() {
-    cout << "Begin fileIO Demonstration"<<endl;
 
-    cout << "Begin Open Write Demo"<<endl;
-    openWrite();
-    openPrint();
-    cout << "End fileIO Demonstration"<<endl;
-    openCloseStreams();
+    std::string inputFilePath, outputFilePath;
+
+    //TODO:
+    // Get input file path
+    //      Enter path of C++ source file (.cpp)
+    // Validate input file path
+    // Get output filepath
+    // Validate output
+    // Open input and output files
+    // Read input file, write to output file with conversions
+    //TODO: figure out <pre> tags and how to write them to file
 
 
-    return 0;
+
 
 }//end main
 
-void openCloseStreams() {
-    cout << "Files Open" << endl;
 
-//declare stream objects
-    ifstream inStream;
-    ofstream outStream;
-
-//connect files to stream objects
-    inStream.open("infile.txt");
-    outStream.open("outfile.txt");
-
-//a shorter version of above
-    ifstream inStream2("infile.txt");
-
-//close resources
-    inStream.close();
-    outStream.close();
-    inStream2.close();
-
-    cout<<"Files closed"<<endl;
-}
-
-void openPrint(){
-    string line;
-    ifstream myFileIn;
-//you may use either .fail() or the .is_open() method
-//of the file stream object
-    myFileIn.open("myFileIn.txt");
-    if(myFileIn.is_open())//or if(myFileIn.fail
-    {
-        cout << "File Open" << endl;
-        while(!myFileIn.eof()){
-            getline(myFileIn,line);
-            cout << line<<endl;
-        }
-        myFileIn.close();
-        cout<<"File closed"<<endl;
-    }else{
-        cout << "Input file failed to open"<< endl;
-    }
-}//end method
-
-void openWrite(){
-    ofstream myFileOut;
-//to append to a file - use ios::app
-    myFileOut.open("myFileOut.txt",ios::app);//open for append
-    if(!myFileOut.fail())//true if it does not fail
-    {
-        myFileOut << "stuff" << " and more stuff" << endl;
-        myFileOut.close();
-        cout << "File Closed"<< endl;
-    }else{
-        cout << "Output File failed to open" << endl;
-    }
-
-}
---
+++ testing.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dylan on 2024-09-22.
+//
+
+#include "testing.h"
